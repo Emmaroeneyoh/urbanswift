5ac81d0cf3e8384f2768f555b37e0fae20860c65
emmaro <emmaro@gmail.com>
2023-11-17 13:19:31 +0100

    first commit

--- user/app/model/route.js
+++ user/app/model/route.js
@@ -3,6 +3,7 @@ const { AssetModel } = require("../../../general/core/db/asset");
 const { OperationModel } = require("../../../general/core/db/route_operation");
 const { Sub_routeModel } = require("../../../general/core/db/sub_route");
 const { TransitModel } = require("../../../general/core/db/transit");
+const { handleError } = require("../../core/utils");
 
 
 const userretrievesinglerouteModel = async (data, res) => {
@@ -25,14 +26,19 @@ const filter_user_operation_model = async (datas, res) => {
     try {
         const { query } = datas;
         
-
         if (query.$and.length == 0) {
-            const userData = await OperationModel.find()
+            const userData = await OperationModel.find().populate({
+              path: "transitid",
+              select: "name address",
+          })
               return userData;
           }
    
       const userData = await OperationModel
-      .find(query)
+      .find(query).populate({
+        path: "transitid",
+        select: "name address",
+    })
      
     
     return userData;
